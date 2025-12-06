19b6783603cc4a34ab1dd913580c6cab69038d62
Silvia Pasquini <daniele.morelli@iisgubbio.edu.it>
2025-12-06 18:02:09 +0100

    Aggiunta funzione ricercaCittà

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+async function ricercaCittà(){
+    let città = document.getElementById('i_Città').value;
+    
+    let risposta = await fetch("http://localhost:8088/cities?city="+città);
+    let listaCittà = await risposta.json();
+    
+    let v = document.getElementById('v_Lista');
+
+    for(let i=0; i < listaCittà.length; i++){
+        
+        let riga = document.createElement('option');
+        riga.value = listaCittà[i].id;
+        riga.innerText = listaCittà[i].city;
+
+        v.appendChild(riga);
+    }
+}
