54b246289600e43f53532d58c4d7fe2bac7fe07a
Silvia Pasquini <daniele.morelli@iisgubbio.edu.it>
2025-12-09 18:06:27 +0100

    aggiunta la funzione per la prenotazione dell'albergo

--- script.js
+++ script.js
@@ -35,28 +35,28 @@ function visualizzaCittà() {
 }
 
 async function aggiorna() {
-    
+
     let valore = v.value;
 
     let risposta = await fetch("http://localhost:8088/cities/" + valore);
     let dett_città = await risposta.json();
-    
+
     n_Città.innerText = dett_città.city;
     let paese = document.getElementById('paese').innerText = dett_città.country;
     let iso = document.getElementById('iso2').innerText = dett_città.iso2;
     let popolazione = document.getElementById('popolazione').innerText = dett_città.population;
 }
 
-function visualizzaPrenotazione(){
+function visualizzaPrenotazione() {
 
     let ricercaCittà = document.getElementById('ricercaCittà');
     ricercaCittà.style.display = 'none';
 
-    città.style.display='none';
+    città.style.display = 'none';
     prenotazione.style.display = 'inline';
 }
 
-function impostaMin(idInput){
+function impostaMin(idInput) {
 
     const data = new Date();
     const anno = data.getFullYear();
@@ -68,3 +68,27 @@ function impostaMin(idInput){
 }
 
 impostaMin("i_DataA");
+
+async function inviaPrenotazione() {
+    let nome = document.getElementById('i_Nome').value;
+    let dataCheckIn = document.getElementById('i_DataA').value;
+    let dataCheckOut = document.getElementById('i_DataR').value;
+    let numP = document.getElementById('i_Persone').value;
+
+    let prenotazione = {
+        cityId: Number(v.value),
+        name: nome,
+        from: dataCheckIn,
+        to: dataCheckOut,
+        guests: Number(numP)
+    };
+
+    let parametriRichiesta = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(prenotazione)
+    };
+    let risposta = await fetch("http://localhost:8088/reservations", parametriRichiesta);
+}
